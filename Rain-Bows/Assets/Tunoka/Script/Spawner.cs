83e4d18dfa9d63599ee0265ea525e065315c53fc
tunomizu <tumikihurai46@gmail.com>
2015-10-29 09:22:18 +0900

    こみ

---
+++ Rain-Bows/Assets/Tunoka/Script/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+    [SerializeField]
+    private GameObject Slime;
+
+    public int MonsterRest;
+    public float SpawnerTime;
+    public float SpawnerInterval;
+
+	// Use this for initialization
+	void Start () 
+    {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+
+        if ( MonsterRest > 0 && SpawnerTime<=0)
+        {
+            MonsterRest--;
+            GameObject slime = (GameObject)Instantiate(Slime, transform.position, transform.rotation);
+        }
+	}
+}
