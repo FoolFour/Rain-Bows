0e579b3718496e4046e6c95db175ea5c5b903104
nishi <icemaunntenn@gmail.com>
2015-11-04 21:36:02 +0900

    ロープスクリプト追加

---
+++ Rain-Bows/Assets/Nishi/Script/CharacterState/Actions/Rope.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rope : ICharaState {
+
+    private float m_Dir;
+    private Parameter m_Prameter;
+
+    public Rope(GameObject obj) : base(obj){ }
+
+    public override void Enter()
+    {
+        m_Prameter = m_GameObject.GetComponent<Parameter>();
+        m_Dir = m_Prameter.dir;
+
+    }
+
+    public override void Action()
+    {
+    }
+
+    public override void Exit()
+    {
+
+    }
+
+    public override void CollisionEnter(Collision2D other)
+    {
+        base.CollisionEnter(other);
+    }
+
+    public override bool IsDead()
+    {
+        return m_isDead;
+    }
+
+    public override StateName Next()
+    {
+        return m_next;
+    }
+}
