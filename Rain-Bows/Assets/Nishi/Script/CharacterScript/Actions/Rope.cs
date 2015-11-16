3b023ae5259e3ad73833197c6ab1952c64b1df09
ralsoon <smallkirby423@yahoo.co.jp>
2015-11-16 18:04:29 +0900

    Rope変更

--- Rain-Bows/Assets/Nishi/Script/CharacterScript/Actions/Rope.cs
+++ Rain-Bows/Assets/Nishi/Script/CharacterScript/Actions/Rope.cs
@@ -5,7 +5,13 @@ public class Rope : ICharaState {
 
     public void Start()
     {
-
+        GameObject gameObj = HitObject.transform.parent.FindChild("DotDrawer").gameObject;
+        Vector3[] path = gameObj.GetComponent<CreatePath>().Path;
+        Hashtable hash = new Hashtable();
+        hash.Add("time", 5.0f);
+        hash.Add("easeType", iTween.EaseType.linear);
+        hash.Add("path", path);
+        iTween.MoveTo(gameObject, hash);
     }
 
     public void Update()
