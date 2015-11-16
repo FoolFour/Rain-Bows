a675ca90063bd4f3d25c7103d5255393937370be
ralsoon <smallkirby423@yahoo.co.jp>
2015-11-16 17:30:38 +0900

    komi

---
+++ Rain-Bows/Assets/Abe/Scripts/Ivy/MouseLineCast.cs
@@ -0,0 +1,37 @@
+﻿// ----- ----- ----- ----- -----
+//
+// MouseLineCast
+//
+// 作成日：
+// 作成者：
+//
+// <概要>
+//
+//
+// ----- ----- ----- ----- -----
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+[AddComponentMenu("Mouse/MouseLineCast")]
+public class MouseLineCast : MouseHitCheck
+{
+    private Vector3 previousPosition;
+
+    protected override void HitAwake()
+    {
+        previousPosition = Input.mousePosition;
+    }
+
+    public override void HitCheck()
+    {
+        isHit = Physics.Linecast(previousPosition, Input.mousePosition, out hitInfo, layer);
+    }
+
+    void LateUpdate()
+    {
+        previousPosition = Input.mousePosition;
+    }
+}
