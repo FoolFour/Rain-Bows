6f94a45e0042bb47f36f7223567742b35797d8ad
tunomizu <tumikihurai46@gmail.com>
2015-11-27 17:11:22 +0900

    種なの設定

---
+++ Rain-Bows/Assets/Tunoka/Script/SoapFlower/SoapSeed.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoapSeed : MonoBehaviour {
+
+    
+
+    [SerializeField]
+    private GameObject SoapFlour;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Space))//ディバック用
+        {
+            Bloom();
+        }
+    }
+
+    public void Bloom()//咲く
+    {
+        GameObject WaterBubble = (GameObject)Instantiate(
+                                       SoapFlour, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
+
