34e7727623c69c3e3acc84cf68dc853af240c7b2
ralsoon <smallkirby423@yahoo.co.jp>
2015-11-27 17:12:08 +0900

    Createパスの中身を変更

--- Rain-Bows/Assets/Abe/Scripts/Ivy/CreatePath.cs
+++ Rain-Bows/Assets/Abe/Scripts/Ivy/CreatePath.cs
@@ -64,7 +64,7 @@ public class CreatePath : MonoBehaviour
     {
         totalDistance         = GetComponent<MouseTotalDistance   >();
         mousePreviousPosition = GetComponent<MousePreviousPosition>();
-        path = new Vector3[pathNumber];
+        path   = new Vector3[pathNumber];
         normal = new Vector3[pathNumber];
     }
 
@@ -72,7 +72,7 @@ public class CreatePath : MonoBehaviour
     void Start()
     {
         nextCreateDistance = 0;
-        arrayNumber = 0;
+        arrayNumber        = 0;
     }
 
     // 更新処理
@@ -98,30 +98,57 @@ public class CreatePath : MonoBehaviour
             for(int i = arrayNumber; i < pathNumber; i++)
             {
                 normal[i] = Vector3.zero;
-                path[i] = transform.position;
+                path[i]   = transform.position;
             }
         }
     }
 
-    public void GetRoute(Vector2 colPoint)
+    public void GetRoute(Vector2 colPoint, int direction)
     {
         int index = GetNearPointIndex(colPoint);
 
         int arrayLength = path.Length - index + 1;
 
         Vector3 n = normal[index];
+
+        //符号をみて符号が
+        //
+        //・合致していれば順番に
+        //・合致しなければ逆順に
+        //
+        //パスを取っていく
+
+        float nSign = Mathf.Sign(n.x);
+        float dSign = Mathf.Sign(direction);
+        
+        Vector3[] array = new Vector3[path.Length - index + 1];
+
+        if(dSign == nSign)
+        {
+            Array.Copy(path, index + 1, array, path.Length, path.Length - index + 1);
+        }
+        else
+        {
+            var path_ = path;
+            Array.Reverse(path_);
+            
+        }
     }
 
     private int GetNearPointIndex(Vector2 colPoint)
     {
+        //if文でnullかどうかを判断するため
         float? minLength = null;
-        int index = 0;
+
+        //breakを通らなかったとき末尾がcolPointに一番近いため
+        int    index     = path.Length - 1;
 
         for(int i = 0; i < path.Length; i++)
         {
             float length;
-            length = VectorUtility.DistanceSquare(path[i].xy(), colPoint);
+            length = VectorUtility.DistanceSquare(path[i].xy(), colPoint); //比較のみ
 
+            //nullかどうかを先に見てエラー回避
             if(!minLength.HasValue || minLength.Value > length)
             {
                 minLength = length;
@@ -130,10 +157,13 @@ public class CreatePath : MonoBehaviour
 
             if(minLength.Value <= length)
             {
+                //minLengthは1つ前のパスの情報になるので
+                //i - 1を返す
                 index = i - 1;
                 break;
             }
         }
+        
         return index;
     }
 	#endregion
